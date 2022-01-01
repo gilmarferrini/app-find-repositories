2b7cad419d65381149b4deed9cb0368e6841ef0d
gilmarferrini <ferrinigilmar@gmail.com>
2022-01-01 09:42:11 -0300

    chore: adicionado model do repositorio e criador do repositorio

---
+++ src/app/repositories/models/repository.ts
@@ -0,0 +1,10 @@
+import { Owner } from './owner';
+
+export interface Repository {
+  name: string;
+  full_name: string;
+  description: string;
+  stargazers_count: number;
+  owner: Owner;
+  updated_at: string;
+}
