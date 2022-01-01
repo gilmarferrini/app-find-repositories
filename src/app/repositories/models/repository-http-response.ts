f048cba12d893bf9ff453758230d111cb5b6ec3b
gilmarferrini <ferrinigilmar@gmail.com>
2022-01-01 09:44:37 -0300

    chore: adicionado model para resposta http da api do github

---
+++ src/app/repositories/models/repository-http-response.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { Repository } from './repository';
+
+export interface RepositoryHttpResponse {
+  total_count: 0;
+  items: Repository[];
+}
